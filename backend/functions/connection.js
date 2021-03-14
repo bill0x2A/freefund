e84ec9ad12f1e7f59d04ddf9a6e188fa5aa1e8d0
Agbanusi <agbanusijohn@gmail.com>
2021-03-14 16:04:50 +0100

    update for chat

--- backend/functions/connection.js
+++ backend/functions/connection.js
@@ -4,11 +4,16 @@ const shortId = require('shortid')
 module.exports = function(io, dbe){
     const db= dbe.collection('CrowdfundProject')
     const user = dbe.collection('CrowdfundUsers')
+    const chats = dbe.collection("CrowdfundChats")
 
     io.on('connection', socket=>{
         console.log("User joined", socket.id )
         
         socket.emit("User", socket.id)
+
+        socket.on("join", data=>{
+            socket.join(data._id)
+        })
 
         // a new contributor to a project
         socket.on("newContributor", (data)=>{
@@ -47,6 +52,30 @@ module.exports = function(io, dbe){
         //project added on the platform
         socket.on("add", (data)=>{
             io.emit("newProjectAdded", data)
+        })
+
+        //new chat created
+        socket.on("createChat", data =>{
+            //socket.join(data.roomId)
+            let id = shortId.generate()
+            user.findOneAndUpdate({address:data.userAddress}, {$push: {$chats: {id,name: data.name} }})
+            chats.insertOne({id, message:[{time:new Date(), chat: data.message}]})
+            user.findOne({address: data.userAddress}, doc =>{
+                socket.to(data._id).emit("send", {chatId:id, message:data.message, temp:socket.id, userId: doc._id, userName: doc.firstName+" "+doc.lastName})
+            })
+            
+        })
+
+        // respond to new chat
+        socket.on("accept", data =>{
+            user.findOneAndUpdate({address:data.userAddress}, {$push: {$chats: {id: data.chatId,name: data.name} }})
+            socket.to(data._id).emit("accepted", data.chatId)
+        })
+
+        // continue chat
+        socket.on("chat", data=>{
+            chats.insertOne({id: data.chatId, message:[{time:new Date(), chat: data.message}]})
+            socket.to(data._id).emit("chatted", {message: data.message, chatId: data.chatId})
         })
 
         // disconnection listener
